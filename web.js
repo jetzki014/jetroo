f947058446ad15789a32f4b96b3d56414c224552
jetzki014 <platajethro11@gmail.com>
2024-06-10 15:57:08 +0800

    Add files via upload

---
+++ web.js
@@ -0,0 +1,30 @@
+function hey() {
+    alert("Product purchased!")
+}
+function ey() {
+    alert("Added to Cart!")
+}
+function change() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
+}
+function theme() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
+}
+function change1() {
+    var img = document.getElementById('picture');
+   img.src='pic2.jpg';
+   
+}
+function change2() {
+    const img = document.getElementById("picture2");
+    img.src= 'pic3.jpg';
+    
+}
+function change3() {
+    const img = document.getElementById("picture3");
+    img.src= 'pic6.jpg';
+}
